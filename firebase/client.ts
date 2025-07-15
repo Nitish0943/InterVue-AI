5261184ec06aa673e7e82d389cd5608384b6c8fb
Nitish0943 <ntshpthk09@gmail.com>
2025-07-15 15:20:23 +0530

    Security: Move Firebase config to environment variables
    - Moved hardcoded Firebase configuration to environment variables
    - Updated firebase/client.ts to use NEXT_PUBLIC_ env vars
    - Standardized Firebase project configuration across the app
    - Enhanced security by removing sensitive data from source code

--- firebase/client.ts
+++ firebase/client.ts
@@ -10,8 +10,8 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_DOMAIN!,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
